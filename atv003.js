d1dd15feb46e376679f8184bb3d16acfa9dffe92
Thiago-vini-gr <thiagaogamerrj@gmail.com>
2025-07-22 08:56:23 -0300

    Add alerts for invalid input and conversion results in dollar converter

--- atv003.js
+++ atv003.js
@@ -1,13 +1,14 @@
 function dolar() {
     let valor = parseFloat(document.getElementById("valor").value);
     let cotacao = 5.58;
-    let resultado = document.getElementById("resultado"); // Declare a variável antes de usá-la
+    let resultado = document.getElementById("resultado");
 
     if (isNaN(valor) || valor <= 0) {
         resultado.innerHTML = "Por favor, insira um valor válido.";
+        alert("Por favor, insira um valor válido.");
     } else {
         let conversao = (valor * cotacao).toFixed(2);
         resultado.innerHTML = `R$ ${conversao}`;
+        alert(`O valor convertido em Reais é: R$ ${conversao}`);
     }
-
 }
